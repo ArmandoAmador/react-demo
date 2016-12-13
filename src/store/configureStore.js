aae2df12550408425dd0ef1c7a17de7c15d28aea
Armando Amador <arm.amador@gmail.com>
2016-12-12 22:06:04 -0500

    mapstatetoprops for course page
     - fix listing errors
     - remove imports not being used

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,5 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore} from 'redux';
 import rootReducer from '../reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 
 export default function configureStore(initialState) {
   return createStore(
